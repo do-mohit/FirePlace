5aea565753339a5b8f9e09784fc3eb1f7bc1491c
mohit <mohitd2288@gmail.com>
2023-01-27 16:33:18 +0530

    added nav

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyATjGNM_IgUB7ORVaWi5DnTMzO4RcJaa_U',
+	authDomain: 'fireplace-47695.firebaseapp.com',
+	projectId: 'fireplace-47695',
+	storageBucket: 'fireplace-47695.appspot.com',
+	messagingSenderId: '745154408430',
+	appId: '1:745154408430:web:84c27dc709c098f91be8ff'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.postcss';
+
+	import Navbar from '../lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
+<slot />
